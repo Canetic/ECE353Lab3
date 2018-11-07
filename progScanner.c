6580e9f60ec2658304f49dd63c1a7ab3ab156016
Tokaena <44303097+Tokaena@users.noreply.github.com>
2018-11-07 00:01:21 -0500

    Update progScanner.c

--- progScanner.c
+++ progScanner.c
@@ -70,6 +70,112 @@ struct inst parser(char *instStr){
 		*/
 }
 
+int regNumberConverter(char *reg)
+{
+	int regNum = -1;
+	char *token;
+	char *delims = " $";
+	token = strtok(reg, delims);
+	printf("\n%s\n", reg);
+	puts(token);
+
+	if(strlen(token) > 2 && strcmp(token, "zero"))
+	{
+		puts("Incorrect register syntax");
+		exit(0);
+	}
+
+	if((atoi(token) <= 25 && atoi(token) >= 10) || !strcmp(token, "8") || !strcmp(token, "9"))
+	{
+		regNum = atoi(token);
+	} else {
+		switch(token[0])
+		{
+		case 's':
+			switch(token[1])
+			{
+			case '0':
+				regNum = 16;
+				break;
+			case '1':
+				regNum = 17;
+				break;
+			case '2':
+				regNum = 18;
+				break;
+			case '3':
+				regNum = 19;
+				break;
+			case '4':
+				regNum = 20;
+				break;
+			case '5':
+				regNum = 21;
+				break;
+			case '6':
+				regNum = 22;
+				break;
+			case '7':
+				regNum = 23;
+				break;
+			default:
+				break;
+			}
+			break;
+		case 't':
+			switch(token[1])
+			{
+			case '0':
+				regNum = 8;
+				break;
+			case '1':
+				regNum = 9;
+				break;
+			case '2':
+				regNum = 10;
+				break;
+			case '3':
+				regNum = 11;
+				break;
+			case '4':
+				regNum = 12;
+				break;
+			case '5':
+				regNum = 13;
+				break;
+			case '6':
+				regNum = 14;
+				break;
+			case '7':
+				regNum = 15;
+				break;
+			case '8':
+				regNum = 24;
+				break;
+			case '9':
+				regNum = 25;
+				break;
+			default:
+				break;
+			}
+			break;
+		case 'z':
+			if(!strcmp(token, "zero"))
+				regNum = 0;
+			break;
+		default:
+			break;
+		}
+	}
+
+	if(regNum == -1)
+	{
+		puts("Incorrect register syntax");
+		exit(0);
+	}
+	return regNum;
+}
+
 int main(int argc, char *argv[]){
 	FILE *input;
 	input = fopen(argv[1], "r");
